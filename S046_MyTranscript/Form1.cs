e08413be0e521ff9f5f83d0d64c74bb987abf8b4
bancha <91675464+bancha96@users.noreply.github.com>
2021-11-02 23:05:11 +0700

    Add class

--- S046_MyTranscript/Form1.cs
+++ S046_MyTranscript/Form1.cs
@@ -17,4 +17,11 @@ namespace S046_MyTranscript
             InitializeComponent();
         }
     }
+    class Course
+    {
+        public string CourseID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCredit { get; set; }
+        public string Grade { get; set; }
+    }
 }
